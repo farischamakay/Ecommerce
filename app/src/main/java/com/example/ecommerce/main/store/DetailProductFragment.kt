bcb84cdcbee68c7fb6f4b44b29e4aefc6fb4ce07
farischamakay <farischamakay09@gmail.com>
2023-09-07 08:34:55 +0700

    cart feature done delete per item

--- app/src/main/java/com/example/ecommerce/main/store/DetailProductFragment.kt
+++ app/src/main/java/com/example/ecommerce/main/store/DetailProductFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.example.ecommerce.R
 import com.example.ecommerce.adapter.ImageDetailAdapter
 import com.example.ecommerce.data.database.Cart
 import com.example.ecommerce.data.models.response.ProductDetailData
