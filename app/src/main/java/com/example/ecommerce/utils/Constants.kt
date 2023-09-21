ceb9d23bf97d51215b724921c0377dd7d5ed89f5
farischamakay <farischamakay09@gmail.com>
2023-09-21 07:39:07 +0700

    remote config done, notification and progress on analytics

--- app/src/main/java/com/example/ecommerce/utils/Constants.kt
+++ app/src/main/java/com/example/ecommerce/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.example.ecommerce.utils
 
 object Constants {
     const val API_KEY = "6f8856ed-9189-488f-9011-0ff4b6c08edc"
-    const val BASE_URL = "http://192.168.129.111:8080"
+    const val BASE_URL = "http://192.168.153.125:5000"
         //tes
 }
